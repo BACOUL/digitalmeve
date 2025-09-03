ff3eabff506c35916c3e2b046002e29eaba034fc
BACOUL <102251510+BACOUL@users.noreply.github.com>
2025-09-03 12:27:52 +0200

    Update test_version.py

--- tests/test_version.py
+++ tests/test_version.py
@@ -1,15 +1,7 @@
-diff --git a/tests/test_version.py b/tests/test_version.py
-index 3180fce..dd9031 100644
---- a/tests/test_version.py
-+++ b/tests/test_version.py
-@@ -1,3 +1,7 @@
- def test_version():
--    import digitalmeve
--
--    assert digitalmeve.__version__ == "1.7.1-dev"
-+    import digitalmeve
-+
-+    # Vérifie que la version exposée par le package
-+    # correspond bien à celle définie dans src/digitalmeve/__init__.py
-+    assert hasattr(digitalmeve, "__version__")
-+    assert digitalmeve.__version__ == "1.7.1-dev"
+def test_version():
+    import digitalmeve
+
+    # Vérifie que la version exposée par le package
+    # correspond bien à celle définie dans src/digitalmeve/__init__.py
+    assert hasattr(digitalmeve, "__version__")
+    assert digitalmeve.__version__ == "1.7.1-dev"
