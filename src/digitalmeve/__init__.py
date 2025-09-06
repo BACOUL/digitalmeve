b3fb130ff60b358192f8ea6e67b601023b6f20aa
BACOUL <102251510+BACOUL@users.noreply.github.com>
2025-09-06 14:28:35 +0200

    Update __init__.py

--- src/digitalmeve/__init__.py
+++ src/digitalmeve/__init__.py
@@ -1,8 +1,9 @@
 from __future__ import annotations
 
 __all__ = ["__version__"]
+
 __version__ = "1.7.1"
 
-# Expose (optionnel) quelques fonctions clés si tu veux depuis le package root.
+# Optionally expose key functions at package root
 # from .generator import generate_meve  # noqa: F401
 # from .verifier import verify_meve     # noqa: F401
